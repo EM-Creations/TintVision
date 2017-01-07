f7ccc98aa2a5ee597e54a5ae8697cbc56a197541
Eddy <eddy@em-creations.co.uk>
2017-01-07 17:51:59 +0000

    New: TintVision main project files initial commit.

---
+++ TintVision/src/com/tintvision/OverlayService.java
@@ -0,0 +1,53 @@
+package com.tintvision;
+
+import com.tintvision.util.FilterView;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+
+public class OverlayService extends Service {
+	private WindowManager windowManager;
+	private View filter;
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// Not used
+		return null;
+	}
+	
+	@Override public void onCreate() {
+    super.onCreate();
+
+    windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+    filter = new FilterView(this); // Create a new view
+    int alpha = 80; // Create alpha variable
+    filter.setBackgroundColor(Color.YELLOW); // Set the background colour to yellow
+    filter.getBackground().setAlpha(alpha); // Set the background's alpha
+    
+    WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        WindowManager.LayoutParams.WRAP_CONTENT,
+        WindowManager.LayoutParams.WRAP_CONTENT,
+        WindowManager.LayoutParams.TYPE_PHONE,
+        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+        PixelFormat.TRANSLUCENT);
+
+    params.gravity = Gravity.TOP | Gravity.START;
+    params.x = 0;
+    params.y = 100;
+
+    windowManager.addView(filter, params);
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    if (filter != null) windowManager.removeView(filter); // If the filter exists, remove it
+  }
+	
+}
---
+++ TintVision/src/com/tintvision/util/FilterView.java
@@ -0,0 +1,12 @@
+package com.tintvision.util;
+
+import android.content.Context;
+import android.view.View;
+
+public class FilterView extends View {
+
+	public FilterView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+}
